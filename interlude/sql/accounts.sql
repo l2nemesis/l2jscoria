6e6d1e2121c132556aba0f8141a86f32fd0e36bf
zenn1989 <zenn1989@cd24fbbe-eb72-4807-afa6-8f0eb2801fde>
2012-09-01 10:59:02 +0000

    fork

--- interlude/sql/accounts.sql
+++ interlude/sql/accounts.sql
@@ -1,15 +1,15 @@
--- ---------------------------
+-- ----------------------------
 -- Table structure for accounts
--- ---------------------------
+-- ----------------------------
 CREATE TABLE IF NOT EXISTS `accounts` (
-  `login` VARCHAR(45) NOT NULL default '',
-  `password` VARCHAR(45) ,
-  `lastactive` DECIMAL(20),
-  `access_level` INT NOT NULL default '0',
-  `lastIP` VARCHAR(20),
-  `lastServer` int(4) default 1,
-  `allowed_ip` varchar(20) NOT NULL default '*',
-  `hwid` VARCHAR(32) NOT NULL default '*',
-  `premium` BINING NOT NULL DEFAULT '0',
+  `login` varchar(45) NOT NULL DEFAULT '',
+  `password` varchar(45),
+  `lastactive` decimal(20,0),
+  `accessLevel` INT NOT NULL DEFAULT '0', 
+  `lastIP` varchar(20),
+  `lastServerId` INT NOT NULL DEFAULT '1',
+  `allowed_ip` varchar(20) NOT NULL DEFAULT '*',
+  `hwid` varchar(250) NOT NULL DEFAULT '*',
+  `premium` BIGINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`login`)
-);
+) DEFAULT CHARSET=utf8;
